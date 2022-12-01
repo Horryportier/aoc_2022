0f0f7b4b11feb820d47f62e187f992a834db3923
Horryportier <kowalskis354@gmail.com>
2022-12-01 19:56:55 +0100

    day one finish

--- python/CalorieCounting.py
+++ python/CalorieCounting.py
@@ -11,15 +11,15 @@ a = [i.split("\n") for i in v if i != '']
 
 i = [[int(j) for j in i if j ] for i in a]
 
-b = [max(i[k]) for k in range(len(i)) if i[k] != []]
+b = [sum(i[k]) for k in range(len(i)) if i[k] != []]
 
 c = max(b)
 
-#print(v)
-#print(a)
-#print(i)
-print(b)
-print(c)
+
+r = sorted(b, reverse=True)
+tree= sum(r[:3])
+
+print('Max {} Max Tree {}'.format(c, tree))
 
 
 
